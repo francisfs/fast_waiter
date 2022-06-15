899420dca47611a4a18e79a3b95bc479545e10c0
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2022-06-15 15:21:00 -0300

    animation_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:fast_waiter/splash.dart';
 import 'package:flutter/material.dart';
 
-import 'home.dart';
+import 'splash.dart';
 
 void main() {
-  runApp(const MyApp(
+  runApp(const Splash(
+    
     //home: Home(),
 
     //debugShowCheckedModeBanner: false, //retirar a barra debug
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const Splash());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListView(
-          children: [
-            // Load a Lottie file from your assets
-            //Lottie.asset('assets/LottieLogo1.json'),
+        body: ListView(children: [
+          // Load a Lottie file from your assets
+          //Lottie.asset('assets/LottieLogo1.json'),
 
-            // Load a Lottie file from a remote url
-            Lottie.network(
-                'https://assets1.lottiefiles.com/private_files/lf30_UlXgnV.json'),
+          // Load a Lottie file from a remote url
 
-            // Load an animation and its images from a zip file
-            //Lottie.asset('assets/lottiefiles/angel.zip'),
-          ],
-        ),
+          Lottie.network(
+              'https://assets1.lottiefiles.com/private_files/lf30_UlXgnV.json')
+
+          // Load an animation and its images from a zip file
+          //Lottie.asset('assets/lottiefiles/angel.zip'),
+        ]),
       ),
     );
   }
